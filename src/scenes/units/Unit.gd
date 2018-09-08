ec470c4c300760e720551bfc963ccc124dbb60ef
Frans Bergström <frabe808@student.liu.se>
2018-09-08 03:20:56 +0200

    added temporary shadow

--- src/scenes/units/Unit.gd
+++ src/scenes/units/Unit.gd
@@ -5,17 +5,20 @@ extends KinematicBody2D
 export (int) var speed = 200
 
 enum LOOK_STATE {TOP_RIGHT, TOP_LEFT, BOTTOM_RIGHT, BOTTOM_LEFT}
-const UNIT_DRAW_LAYER = 1
 
+const UNIT_DRAW_LAYER = 2
+const SHADOW_DRAW_LAYER = 1
+
+onready var body = $BodyPivot/Body
+onready var shadow = $Shadow
 onready var health = $Health
+
 var look_state = TOP_RIGHT
 var look_position = Vector2(0,0)
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	$BodyPivot/Body.z_index = UNIT_DRAW_LAYER
-	pass
+	body.z_index = UNIT_DRAW_LAYER
+	shadow.z_index = SHADOW_DRAW_LAYER
 
 func get_movement_direction():
 	pass
@@ -24,19 +27,15 @@ func get_aim_position():
 	pass
 
 func _set_look_state (look_position):
-	var body_global_pos = $BodyPivot/Body.global_position
-	if look_position.x > body_global_pos.x:
-		look_state = BOTTOM_RIGHT if look_position.y > body_global_pos.y else TOP_RIGHT
+	if look_position.x > body.global_position.x:
+		look_state = BOTTOM_RIGHT if look_position.y > body.global_position.y else TOP_RIGHT
 	else:
-		look_state = BOTTOM_LEFT if look_position.y > body_global_pos.y else TOP_LEFT
+		look_state = BOTTOM_LEFT if look_position.y > body.global_position.y else TOP_LEFT
 
 func set_dead(value):
 	set_process_input(not value)
 	set_physics_process(not value)
 
 func _physics_process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
 	_set_look_state(get_aim_position())
-	pass
 	
--- src/scenes/units/states/motion.gd
+++ src/scenes/units/states/motion.gd
@@ -5,5 +5,7 @@ var velocity = Vector2()
 
 func update(delta):
     var body_sprite = owner.get_node("BodyPivot/Body")
+    var shadow_sprite = owner.get_node("Shadow")
     var status = owner.look_state == owner.BOTTOM_LEFT or owner.look_state == owner.TOP_LEFT
     body_sprite.set_flip_h(status)
+    shadow_sprite.set_flip_h(status)
