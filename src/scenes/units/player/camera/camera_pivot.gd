df1834749a54f18085878206f757e730334d92f4
Frans Bergström <frabe808@student.liu.se>
2018-09-28 00:02:59 +0200

    better action camera

--- src/scenes/units/player/camera/camera_pivot.gd
+++ src/scenes/units/player/camera/camera_pivot.gd
@@ -1,14 +1,15 @@
 extends Position2D
 
 onready var player = $".."
-
+onready var offset = Vector2(0, 10)
 # TODO: Implement "action" camera
 
 func _ready():
 	pass
 
 func _process(delta):
-	var mouse_pos = get_global_mouse_position()
+	var pos = (get_global_mouse_position() + player.global_position)/4
+	global_position = pos
 
 func update_pivot_angle():
 	pass
